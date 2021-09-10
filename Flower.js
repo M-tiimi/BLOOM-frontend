0c03136bef6cfc9ebbc2dc8eb0bb8acab7e41351
Juulia Jokinen <bgm030@myy.haaga-helia.fi>
2021-09-10 13:34:18 +0300

    Added alert

--- Flower.js
+++ Flower.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
 
 export default function Flower() {
+
+  const showAlert = () =>
+  Alert.alert(
+    "Alert Title",
+    "Mitä kuuluu?",
+    [
+      {
+        text: "Cancel",
+        onPress: () => Alert.alert("Cancel Pressed"),
+        style: "cancel",
+      },
+    ],
+    {
+      cancelable: true,
+      onDismiss: () =>
+        Alert.alert(
+          "This alert was dismissed by tapping outside of the alert dialog."
+        ),
+    }
+  );
+
   return (
     <View style={styles.container}>
+      <View style={styles.container}>
+    <Button title="Show alert" onPress={showAlert} />
+  </View>
       <Text>Grow your flower!</Text>
-      <Text>Ansku</Text>
       <StatusBar style="auto" />
       <Image
         style={styles.imageContainer}
